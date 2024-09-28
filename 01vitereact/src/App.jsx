c0e19bb331169be92e5981e00a55df32f2b9fa69
iamrudra0110 <rudraashtikar010@gmail.com>
2024-09-28 12:12:09 +0530

    react with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,26 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Hey there, Hello world!|RA|</h1>
+      <ol>
+        <li>start off with "npm create vite@latest"</li>
+        <li>then vite will ask for a name type your "desired filename"</li>
+        <li>
+          vite will ask in which library do you want to use select "React"
+        </li>
+        <li>then select "javascript" from the displayed options</li>
+        <li>Your vite+react folder is ready</li>
+        <li>Go inside the created file using "cd filename"</li>
+        <li>
+          then type npm install in the terminal to install node modules 8. Type
+          "npm run dev" to fire up the react app
+        </li>
+      </ol>
+    </>
+  );
+}
+// {anything} written in curly braces disturbs the site and it does not work
+export default App;
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,16 @@
+function Chai() {
+  return (
+    <>
+      <h1>Chai in react</h1>
+      <p>
+        To write this kind of components in Vite proceed carefully as if u
+        delete everything immediately vite will not work what you should do
+        instead is delete these things one by one and see if the site is still
+        working like when i deleted the react {"state"} command my site crashed
+        and was not working so delete things one by one
+      </p>
+    </>
+  );
+}
+export default Chai;
+// .jsx means that this is a component file
