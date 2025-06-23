7d50d08d4e4271825bb1b740281c0c7c17639ed9
iamrudra0110 <rudraashtikar010@gmail.com>
2025-06-23 22:29:02 +0530

    props, important for interview

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Rudra",
+    age: 21,
+  };
+
+  let newArr = [1, 2, 3];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="bg-green-400 text-black p-4 rounded mb-4 ">Rudra</h1>
+      <Card
+        username={"chaiaurcode"}
+        btnText="click ME"
+        image={
+          "https://media.istockphoto.com/id/1497809928/photo/systematical-planning-work-increase-production-capacity-and-reduce-costs-production-planning.jpg?s=612x612&w=is&k=20&c=1ZCougiD4FiPNWDitsXkiCzGKpEVgV7WCG_8HVRCQQo="
+        }
+      />
+      <Card
+        username={"bob"}
+        btnText="tap ME"
+        image={
+          "https://plus.unsplash.com/premium_photo-1675431443027-ad1f46c93c8d?q=80&w=832&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
