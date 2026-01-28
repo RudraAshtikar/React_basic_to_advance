fc9326a13eb2b4046feb287136e77ab052fb8d26
iamrudra0110 <rudraashtikar010@gmail.com>
2026-01-28 16:40:36 +0530

    Fix case sensitivity issues

---
+++ 10todoContextLocal/src/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TodoProvider } from "./context";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
+    );
+  }; // I have to find ki konsa aisa todo hei jo id se match kar raha ho taki mei uske andar naya todo add kar saku
+
+  // start understanding this deleteTodo logic at 8.30 : not done
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo,
+      ),
+    );
+  };
+
+  //Basic context functionality has ended here, from here we'll start our local storage functionality
+
+  //Start localStorage Functionality at 23-1 / 7.00 pm --not done
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+  //JSON will keep the structure preserved that's why we want these values in JSON
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App;
