02ff32a8ee5a9fc5eab2617d03ed4d75d4f0aa88
iamrudra0110 <rudraashtikar010@gmail.com>
2026-02-03 15:27:49 +0530

    todo functionalities

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./context";
+import TodoForm from "./components/TodoFormm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -20,6 +22,7 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
+  // This deleteTodo functionality works by filtering out the matching id and making a new array where there's not the matching id
 
   const toggleComplete = (id) => {
     setTodos((prev) =>
@@ -57,9 +60,16 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- 10todoContextLocal/src/components/TodoFormm.jsx
+++ 10todoContextLocal/src/components/TodoFormm.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import { useTodo } from "../context/TodoContext";
 
 function TodoForm() {
   const [todo, setTodo] = useState("");
 
-  const { addTodo } = todo;
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+
+    if (!todo) returns;
+
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
 
   return (
-    <form className="flex">
+    <form onSubmit={add} className="flex">
       <input
         type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
       />
